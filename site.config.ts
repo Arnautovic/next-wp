fa59c701e9a147da0552b1213d7244443fcb890d
Arnautovic <dizajnerweb@gmail.com>
2025-07-02 20:56:24 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_name: "Fast site by arnautcode",
+  site_name: "Creative Minds. Powerful Code.",
   site_description: "Fast Wordpress theme for presentation. ",
   site_domain: "https://arnautcode.com",
 };
